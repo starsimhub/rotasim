37e9df731feb0b710f4203a53aebb0dfcbf9489e
Cliff Kerr <questionably@gmail.com>
2024-11-21 10:17:48 -0500

    update test calib with full run, 315 s, 10k inci

--- test_calibration.py
+++ test_calibration.py
@@ -2,11 +2,7 @@ import sciris as sc
 import rotaABM as rabm
 import calibration.process_incidence as cpi
 
-rota = rabm.RotaABM(N=10_000, timelimit=2)
-events = rota.run()
-cols = rota.results.columns
-res = rota.results.infected_all
-df = sc.dataframe(data=res, columns=cols)
-
-out = cpi.process_model(df)
+sim = rabm.RotaABM(N=100_000, timelimit=8)
+events = sim.run()
+out = cpi.process_model(sim.df)
 print(out)
