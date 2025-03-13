44495081dcd62555ad14a7777256d8358b50b4eb
Cliff Kerr <questionably@gmail.com>
2025-03-13 14:21:16 -0400

    add plot_results file

--- plot_results.py
+++ plot_results.py
@@ -8,15 +8,13 @@ import matplotlib.pyplot as plt
 
 T = sc.timer()
 
-fns = dict(
-    old = 'results/rota_strains_infected_all_test_1_0.1_1_1_1_0_0.5_1.csv',
-    new = 'results/rota_strains_infected_all_1_0.1_1_1_1_0_0.5_1.csv'
-)
+fns = sc.getfilelist(folder='results', pattern='rota_strains_infected_all*.csv')
 
 dfs = sc.objdict()
 rrs = sc.objdict()
 
-for key,fn in fns.items():
+for fn in fns:
+    key = fn
     res = sc.objdict()
     df = sc.dataframe.read_csv(fn)
     dfs[key] = df
@@ -36,6 +34,9 @@ for i,key,res in rrs.enumitems():
     plt.subplot(2,1,i+1)
     for strain in res.strains:
         plt.plot(res.x, res.y[strain], 'o-', label=strain, alpha=0.5)
+    plt.title(key)
     plt.legend()
+
+plt.show()
 
 T.toc()
