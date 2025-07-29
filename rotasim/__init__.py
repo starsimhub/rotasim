4454731b36059fd098a9ff8059e6b2d1110a9877
Daniel Klein <daniel.klein@gatesfoundation.org>
2025-07-29 00:50:59 -0700

    Export new analyzers from package __init__.py
    Added EventStats, AgeStats analyzers to package exports alongside
    existing StrainStats analyzer for complete v1 CSV compatibility.

--- rotasim/__init__.py
+++ rotasim/__init__.py
@@ -2,5 +2,6 @@ from .version import __version__, __versiondate__
 from .rotavirus import *
 from .immunity import *
 from .reassortment import *
+from .analyzers import *
 from .utils import *
 from .rotasim import *
