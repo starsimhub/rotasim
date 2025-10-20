4308f58400b2714b7f69c963c68d76405a581b0a
RHull-IDM <ryan.hull@gatesfoundation.org>
2025-10-20 11:21:01 -0700

    updated version

--- rotasim/version.py
+++ rotasim/version.py
@@ -4,5 +4,5 @@ Version and license information.
 
 __all__ = ["__version__", "__versiondate__"]
 
-__version__ = "0.1.0"
-__versiondate__ = "2025-04-11"
+__version__ = "0.2.0"
+__versiondate__ = "2025-10-20"
